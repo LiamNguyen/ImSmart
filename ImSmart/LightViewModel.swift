e576a414ab79787f46c0645f837df02d80b2e8b2
Thien Phuc Nguyen <phuc.nguyen2@metropolia.fi>
2017-04-05 18:01:18 +0300

    Implement LightModel, LightViewModel and Add unit test for light creation

---
+++ ImSmart/LightViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  LightViewModel.swift
+//  ImSmart
+//
+//  Created by Cao Do Nguyen on /04/04/2017.
+//  Copyright © 2017 LetsDev. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class LightViewModel {
+    fileprivate let light: Light!
+    
+    var isOn        = Variable<Bool>(false)
+    var brightness  : Variable<Int>!
+    var area        : Variable<String>!
+    
+    private let disposalBag = DisposeBag()
+    
+    init(light: Light) {
+        self.light      = light
+        
+        self.brightness = Variable<Int>(self.light.brightness)
+        self.area       = Variable<String>(self.light.area)
+        
+        bindRx()
+    }
+    
+    private func bindRx() {
+        isOn.asObservable()
+            .subscribe(onNext: { isOn in
+                self.light.isOn = isOn
+            }).addDisposableTo(disposalBag)
+        
+        brightness.asObservable()
+            .filter({ brightness -> Bool in
+                return self.isOn.value ? true : false
+            })
+            .subscribe(onNext: { brightness in
+                self.light.brightness = brightness
+            })
+            .addDisposableTo(disposalBag)
+        
+        area.asObservable()
+            .subscribe(onNext: { area in
+                self.light.area = area
+            }).addDisposableTo(disposalBag)
+    }
+}
---
+++ ImSmartTests/LightViewModelTests.swift
@@ -0,0 +1,50 @@
+//
+//  LightTests.swift
+//  ImSmart
+//
+//  Created by Cao Do Nguyen on /04/04/2017.
+//  Copyright © 2017 LetsDev. All rights reserved.
+//
+
+import XCTest
+@testable import ImSmart
+
+class LightViewModelTests: XCTestCase {
+    
+    var kitchenLight: Light!
+    var kitchenLightViewModel: LightViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        
+        kitchenLight = Light(brightness: 50, area: "Kitchen")
+        kitchenLightViewModel = LightViewModel(light: kitchenLight)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testLightModelUpdate() {
+        kitchenLightViewModel.isOn.value = true
+        kitchenLightViewModel.brightness.value = 100
+        kitchenLightViewModel.area.value = "Living room"
+        
+        XCTAssertEqual(kitchenLight.isOn, true)
+        XCTAssertEqual(kitchenLight.brightness, 100)
+        XCTAssertEqual(kitchenLight.area, "Living room")
+    }
+}
